0ee918cf8c8922ac99149ce28c1dbc0bdb91abfe
Kamesh Sampath <kamesh.sampath@hotmail.com>
2024-09-26 16:52:35 +0530

    (fix): use db and schema with secret example

--- git_integration.sql
+++ git_integration.sql
@@ -4,17 +4,28 @@ USE WAREHOUSE KAMESH_DEMOS_S;
 
 CREATE OR REPLACE DATABASE KAMESH_GIT_REPOS;
 
+USE DATABASE KAMESH_GIT_REPOS;
+
 -- Create the API Integration
 CREATE API INTEGRATION IF NOT EXISTS  kameshsampath_git
     API_PROVIDER = git_https_api
     -- allowed orgs and repositories
     API_ALLOWED_PREFIXES = ('https://github.com/kameshsampath')
-    -- ADD secret to use private repositories
     ENABLED = TRUE;
+
+CREATE OR REPLACE SCHEMA REPOS;
+USE REPOS;
+
+-- CREATE OR REPLACE SECRET my_gh_token
+--   TYPE = password
+--   USERNAME = 'kameshsampath'
+--   PASSWORD = 'ghp_token';
 
 -- Create Repository - this will be a stage in Snowflake
 CREATE GIT REPOSITORY IF NOT EXISTS git_integration_demo
     API_INTEGRATION = kameshsampath_git
+    -- ADD secret to use private repositories
+    -- GIT_CREDENTIALS = my_gh_token
     ORIGIN = 'https://github.com/kameshsampath/sf-git-integration-demo.git';
 
 -- Refresh repoistory
@@ -26,3 +37,6 @@ LIST @git_integration_demo/branches/main;
 -- Run the sql `demo.sql` from main branch root
 EXECUTE IMMEDIATE FROM @git_integration_demo/branches/main/demo.sql;
 
+
+-- Run the sql `cleaup.sql` from main branch root to clean demo objects
+-- EXECUTE IMMEDIATE FROM @git_integration_demo/branches/main/demo.sql;
